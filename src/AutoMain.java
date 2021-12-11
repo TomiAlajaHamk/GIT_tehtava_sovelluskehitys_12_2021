d923a13d96f686af824c6328390fe0999793ea26
Tomi Alaja <tomi.alaja@student.hamk.fi>
2021-12-11 14:30:22 +0200

    Commit 1

--- src/AutoMain.java
+++ src/AutoMain.java
@@ -3,8 +3,31 @@ public class AutoMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ii
+		
 
 	}
 
+}
+class Auto
+{
+	public String merkki;
+	public String malli;
+	public int bensanMaara;
+	
+
+	public void Jarruta()
+	{
+		
+	}
+	
+	public void Kiihdyta()
+	{
+		
+	}
+	
+	public void NaytaTiedot()
+	{
+		
+	}
+	
 }
