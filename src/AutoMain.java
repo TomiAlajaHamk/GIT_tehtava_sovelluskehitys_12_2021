40bc25d7c4e75ef1e0bd2488a5ee2b0647e9b70e
Tomi Alaja <tomi.alaja@student.hamk.fi>
2021-12-11 16:11:17 +0200

    Commit 5 (final)

--- src/AutoMain.java
+++ src/AutoMain.java
@@ -6,8 +6,12 @@ public class AutoMain {
 		
 	Auto auto1 = new Auto();	
 	auto1.NaytaTiedot();
-	auto1.Kiihdyta();
-	auto1.NaytaTiedot();
+//	auto1.Kiihdyta();
+//	auto1.NaytaTiedot();
+//	auto1.tankkaa(8);
+	
+	Auto auto2 = new Auto("Honda", "Jazz", 13);
+	
 	}
 
 }
@@ -23,8 +27,16 @@ class Auto
 	{
 		merkki = "Toyota";
 		malli = "Yaris";
-		bensanMaara = 0;
-
+		bensanMaara = 10;
+	}
+	
+	public Auto(String autonMerkki, String autonMalli, int bensaaTankissa)
+	{
+		merkki = autonMerkki;
+		malli = autonMalli;
+		bensanMaara = bensaaTankissa;
+		NaytaTiedot();
+				
 	}
 	
 	public void Jarruta()
@@ -55,6 +67,16 @@ class Auto
 		System.out.println("Model: " + malli);
 		System.out.println("Fuel: " + bensanMaara);
 	}
+	
+	public void tankkaa (int tankattuBensa)
+	{
+		System.out.println("Tankissa bensaa: " + bensanMaara);
+		System.out.println("Bensaa tankataan: " + tankattuBensa);
+		System.out.println("Bensaa tankissa tankkauksen jälkeen: " +(bensanMaara+tankattuBensa));
+	}
+	
+	
+	
 	
 }
 
